798ab1df8b56c144d0db2ec1c2a5fe40853ce907
Christian <christian.perzy@gmail.com>
2024-11-19 14:54:07 +0000

    Save progress: 1.1

--- queries.sh
+++ queries.sh
@@ -11,31 +11,31 @@ echo -e "\nTotal number of goals in all games from both teams combined:"
 echo "$($PSQL "SELECT (SUM(winner_goals) + SUM(opponent_goals)) as sum_goals FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams:"
-echo "$($PSQL "SELCT AVG(winner_goals) FROM games")"
+echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo "$($PSQL "SELCT ROUND(AVG(winner_goals), 2) FROM games")"
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo "$($PSQL SELECT AVG(winner_goals + opponent_goals) FROM games")"
+echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo "$($PSQL "SELCT MAX(winner_goals) FROM games")"
+echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo "$($PSQL "SELCT COUNT(*) FROM games WHERE winner_goals > 2")"
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo "($PSQL "SELECT name FROM games INNER JOIN teams ON winner_id = team_id WHERE year = 2018 AND round ='Final'")"
+echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON winner_id = team_id WHERE year = 2018 AND round ='Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "($PSQL "SELECT name FROM teams INNER JOIN games AS gw ON team_id = gw.winner_id INNER JOIN games AS go ON team_id = go.opponent_id WHERE gw.year = 2014 AND gw.round = 'Eighth-Final' GROUP BY name")"
+echo "$($PSQL "SELECT teams.name FROM games INNER JOIN teams ON games.winner_id = teams.team_id WHERE round='Eighth-Final' AND year=2014 UNION SELECT teams.name FROM games INNER JOIN teams ON games.opponent_id = teams.team_id WHERE round='Eighth-Final' AND year=2014 ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo "($PSQL "SELECT name FROM teams INNER JOIN games ON team_id = winner_id GROUP BY name")"
+echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON team_id = winner_id GROUP BY name ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "($PSQL "SELECT year, name FROM games INNER JOIN teams  ON team_id = winner_id WHERE round = 'Final'")"
+echo "$($PSQL "SELECT year, name FROM games INNER JOIN teams  ON team_id = winner_id WHERE round = 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo "($PSQL "SELECT * FROM teams WHERE name LIKE 'Co%'")"
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
--- worldcup.sql
+++ worldcup.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.21 (Debian 12.21-1.pgdg120+1)
--- Dumped by pg_dump version 14.13 (Ubuntu 14.13-0ubuntu0.22.04.1)
+-- Dumped from database version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
+-- Dumped by pg_dump version 12.17 (Ubuntu 12.17-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -134,40 +134,82 @@ ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.t
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.games VALUES (1, 2018, 'Final', 1, 2, 4, 2);
-INSERT INTO public.games VALUES (2, 2018, 'Third Place', 3, 4, 2, 0);
-INSERT INTO public.games VALUES (3, 2018, 'Semi-Final', 2, 4, 2, 1);
-INSERT INTO public.games VALUES (4, 2018, 'Semi-Final', 1, 3, 1, 0);
-INSERT INTO public.games VALUES (5, 2018, 'Quarter-Final', 2, 5, 3, 2);
-INSERT INTO public.games VALUES (6, 2018, 'Quarter-Final', 4, 6, 2, 0);
-INSERT INTO public.games VALUES (7, 2018, 'Quarter-Final', 3, 7, 2, 1);
+INSERT INTO public.games VALUES (8, 2018, 'Final', 8, 9, 4, 2);
+INSERT INTO public.games VALUES (9, 2018, 'Third Place', 10, 11, 2, 0);
+INSERT INTO public.games VALUES (10, 2018, 'Semi-Final', 9, 11, 2, 1);
+INSERT INTO public.games VALUES (11, 2018, 'Semi-Final', 8, 10, 1, 0);
+INSERT INTO public.games VALUES (12, 2018, 'Quarter-Final', 9, 12, 3, 2);
+INSERT INTO public.games VALUES (13, 2018, 'Quarter-Final', 11, 13, 2, 0);
+INSERT INTO public.games VALUES (14, 2018, 'Quarter-Final', 10, 14, 2, 1);
+INSERT INTO public.games VALUES (15, 2018, 'Quarter-Final', 8, 15, 2, 0);
+INSERT INTO public.games VALUES (16, 2018, 'Eighth-Final', 11, 16, 2, 1);
+INSERT INTO public.games VALUES (17, 2018, 'Eighth-Final', 13, 17, 1, 0);
+INSERT INTO public.games VALUES (18, 2018, 'Eighth-Final', 10, 18, 3, 2);
+INSERT INTO public.games VALUES (19, 2018, 'Eighth-Final', 14, 19, 2, 0);
+INSERT INTO public.games VALUES (20, 2018, 'Eighth-Final', 9, 20, 2, 1);
+INSERT INTO public.games VALUES (21, 2018, 'Eighth-Final', 12, 21, 2, 1);
+INSERT INTO public.games VALUES (22, 2018, 'Eighth-Final', 15, 22, 2, 1);
+INSERT INTO public.games VALUES (23, 2018, 'Eighth-Final', 8, 23, 4, 3);
+INSERT INTO public.games VALUES (24, 2014, 'Final', 24, 23, 1, 0);
+INSERT INTO public.games VALUES (25, 2014, 'Third Place', 25, 14, 3, 0);
+INSERT INTO public.games VALUES (26, 2014, 'Semi-Final', 23, 25, 1, 0);
+INSERT INTO public.games VALUES (27, 2014, 'Semi-Final', 24, 14, 7, 1);
+INSERT INTO public.games VALUES (28, 2014, 'Quarter-Final', 25, 26, 1, 0);
+INSERT INTO public.games VALUES (29, 2014, 'Quarter-Final', 23, 10, 1, 0);
+INSERT INTO public.games VALUES (30, 2014, 'Quarter-Final', 14, 16, 2, 1);
+INSERT INTO public.games VALUES (31, 2014, 'Quarter-Final', 24, 8, 1, 0);
+INSERT INTO public.games VALUES (32, 2014, 'Eighth-Final', 14, 27, 2, 1);
+INSERT INTO public.games VALUES (33, 2014, 'Eighth-Final', 16, 15, 2, 0);
+INSERT INTO public.games VALUES (34, 2014, 'Eighth-Final', 8, 28, 2, 0);
+INSERT INTO public.games VALUES (35, 2014, 'Eighth-Final', 24, 29, 2, 1);
+INSERT INTO public.games VALUES (36, 2014, 'Eighth-Final', 25, 19, 2, 1);
+INSERT INTO public.games VALUES (37, 2014, 'Eighth-Final', 26, 30, 2, 1);
+INSERT INTO public.games VALUES (38, 2014, 'Eighth-Final', 23, 17, 1, 0);
+INSERT INTO public.games VALUES (39, 2014, 'Eighth-Final', 10, 31, 2, 1);
 
 
 --
 -- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.teams VALUES (1, 'France');
-INSERT INTO public.teams VALUES (2, 'Croatia');
-INSERT INTO public.teams VALUES (3, 'Belgium');
-INSERT INTO public.teams VALUES (4, 'England');
-INSERT INTO public.teams VALUES (5, 'Russia');
-INSERT INTO public.teams VALUES (6, 'Sweden');
-INSERT INTO public.teams VALUES (7, 'Brazil');
+INSERT INTO public.teams VALUES (8, 'France');
+INSERT INTO public.teams VALUES (9, 'Croatia');
+INSERT INTO public.teams VALUES (10, 'Belgium');
+INSERT INTO public.teams VALUES (11, 'England');
+INSERT INTO public.teams VALUES (12, 'Russia');
+INSERT INTO public.teams VALUES (13, 'Sweden');
+INSERT INTO public.teams VALUES (14, 'Brazil');
+INSERT INTO public.teams VALUES (15, 'Uruguay');
+INSERT INTO public.teams VALUES (16, 'Colombia');
+INSERT INTO public.teams VALUES (17, 'Switzerland');
+INSERT INTO public.teams VALUES (18, 'Japan');
+INSERT INTO public.teams VALUES (19, 'Mexico');
+INSERT INTO public.teams VALUES (20, 'Denmark');
+INSERT INTO public.teams VALUES (21, 'Spain');
+INSERT INTO public.teams VALUES (22, 'Portugal');
+INSERT INTO public.teams VALUES (23, 'Argentina');
+INSERT INTO public.teams VALUES (24, 'Germany');
+INSERT INTO public.teams VALUES (25, 'Netherlands');
+INSERT INTO public.teams VALUES (26, 'Costa Rica');
+INSERT INTO public.teams VALUES (27, 'Chile');
+INSERT INTO public.teams VALUES (28, 'Nigeria');
+INSERT INTO public.teams VALUES (29, 'Algeria');
+INSERT INTO public.teams VALUES (30, 'Greece');
+INSERT INTO public.teams VALUES (31, 'United States');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 7, true);
+SELECT pg_catalog.setval('public.games_game_id_seq', 39, true);
 
 
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.teams_team_id_seq', 7, true);
+SELECT pg_catalog.setval('public.teams_team_id_seq', 31, true);
 
 
 --
